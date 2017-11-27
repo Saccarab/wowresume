b84375a993d079f7676597ebbead0badcdad7aa3
Ali San Kaya <akaya2@binghamton.edu>
2017-11-27 12:01:36 -0500

    execute flow

--- src/fetchData.js
+++ src/fetchData.js
@@ -438,7 +438,13 @@ function rankings(){
 
 			confirmMigrate()
 
-			fill();
+			if (guildRequestList.length !== 0)
+				fill();
+			else{
+				notLoading()
+				process = false; //end process
+			}
+
 		},
 		error: function(){
 			clicked = false;	  		
